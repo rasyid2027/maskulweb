6a31924f755edca7dd22fab9dbea181d0d3ddfa8
rasyid2027 <halimurrasyid56@gmail.com>
2020-10-26 13:31:13 +0700

    modificate master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
 	<!-- ICONS -->
 	<link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
-	<link rel="icon" type="image/png" sizes="96x96" href="{{asset('assets/img/favicon.png')}}">
+	{{-- <link rel="icon" type="image/png" sizes="96x96" href="{{asset('assets/img/favicon.png')}}"> --}}
+	<link rel="icon" type="image/png" sizes="32x32" href="{{asset('/favicon-32x32.png')}}">
 </head>
 
 <body>
@@ -33,7 +34,7 @@
 		<!-- MAIN -->
 		@yield('content')
 		<!-- END MAIN -->
-		<div class="clearfix"></div>
+		{{-- <div class="clearfix"></div> --}}
 		<footer>
 			<div class="container-fluid">
 				<p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
